a2d2d5a7f077fe917b04bff5c11d603b3b1a73c9
BogdanZsolt <bogdan.zsolt.m@gmail.com>
2023-03-14 07:53:45 +0100

    code formatting.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import {tns} from 'tiny-slider';
+import { tns } from 'tiny-slider';
 import './index.scss';
 import './style.scss';
 
-const slider = tns({
-  container: ".slides",
-  mode: "gallery",
-  items: 1,
-  slideBy: "page",
-  controls: false,
-  nav: SLIDER_OPTIONS.controlNav,
-  navPosition: "bottom",
-  touch: true,
-  speed: 400,
-  autoplay: true,
-  autoplayButtonOutput: false,
-  autoplayPosition: "bottom",
-  autoplayTimeout: 7000,
-});
+const slider = tns( {
+	container: '.slides',
+	mode: 'gallery',
+	items: 1,
+	slideBy: 'page',
+	controls: false,
+	nav: SLIDER_OPTIONS.controlNav,
+	navPosition: 'bottom',
+	touch: true,
+	speed: 400,
+	autoplay: true,
+	autoplayButtonOutput: false,
+	autoplayPosition: 'bottom',
+	autoplayTimeout: 7000,
+} );
